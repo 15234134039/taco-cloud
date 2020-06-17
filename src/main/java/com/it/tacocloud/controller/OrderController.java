19d4b9f982af1dd0e24c4552749588eff485d5d8
gxp <1551039419@qq.com>
2020-06-17 17:15:01 +0800

    Spring实战(第5版)笔记_02

--- src/main/java/com/it/tacocloud/controller/OrderController.java
+++ src/main/java/com/it/tacocloud/controller/OrderController.java
@@ -1,13 +1,17 @@
 package com.it.tacocloud.controller;
 
+import com.it.tacocloud.data.OrderRepository;
 import com.it.tacocloud.pojo.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
@@ -19,7 +23,15 @@ import javax.validation.Valid;
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+
+    private OrderRepository orderRepo;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 
     @GetMapping("/current")
     public String orderForm(Model model) {
@@ -28,11 +40,15 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
         log.info("Order submitted: " + order);
+        orderRepo.save(order);
+
+        //重置session
+        sessionStatus.setComplete();
         return "redirect:/";
     }
 
