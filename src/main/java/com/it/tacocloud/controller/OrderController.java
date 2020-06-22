cbb25ceea967a9ca68e022c672e2d09496729e5b
gxp <1551039419@qq.com>
2020-06-22 15:45:58 +0800

    配置属性

--- src/main/java/com/it/tacocloud/controller/OrderController.java
+++ src/main/java/com/it/tacocloud/controller/OrderController.java
@@ -2,14 +2,21 @@ package com.it.tacocloud.controller;
 
 import com.it.tacocloud.pojo.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jni.User;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.validation.Valid;
+
 
 /**
  * @author gxp
@@ -19,7 +26,15 @@ import javax.validation.Valid;
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
+
+    private int pageSize = 20;
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
 
     @GetMapping("/current")
     public String orderForm(Model model) {
@@ -34,6 +49,12 @@ public class OrderController {
         }
         log.info("Order submitted: " + order);
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, pageSize);
+        return "";
     }
 
 
